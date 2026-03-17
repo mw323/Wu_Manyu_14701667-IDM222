8a4340a367bf9c821d22209f5fff37fc49bb3352
Manyu Wu <mw3493@drexel.edu>
2026-03-17 15:38:15 -0400

    Add slideshow function

--- main.js
+++ main.js
@@ -13,3 +13,32 @@ closeBtn.addEventListener("click", () => {
     closeBtn.classList.toggle("closeIcon");
     menu.classList.toggle("hidden");
 });
+// slideshow
+const slides = document.querySelectorAll(".demo_slide");
+const nextBtn = document.querySelector(".next_btn");
+const prevBtn = document.querySelector(".prev_btn");
+
+let currentSlide = 0;
+
+function showSlide(index) {
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+    });
+
+    slides[index].classList.add("active");
+}
+nextBtn.addEventListener("click", () => {
+    currentSlide++;
+    if (currentSlide >= slides.length) {
+        currentSlide = 0;
+    }
+    showSlide(currentSlide);
+});
+
+prevBtn.addEventListener("click", () => {
+    currentSlide--;
+    if (currentSlide < 0) {
+        currentSlide = slides.length - 1;
+    }
+    showSlide(currentSlide);
+});
